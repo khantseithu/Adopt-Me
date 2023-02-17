ace45dcca791eefe92eaf8aae85343c1bed4e64b
khantseithu <khantseithuu@gmail.com>
2023-02-17 11:00:04 +0630

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const Pet = (props) => {
+    return React.createElement("div", null, [
+        React.createElement("h1", null, props.name),
+        React.createElement("h2", null, props.animal),
+        React.createElement("h2", null, props.breed)
+    ])
+}
+const App = () => {
+    return React.createElement("div", null, [
+        React.createElement("h1", null, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            name: "Max",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, {
+            name: "Doint",
+            animal: "cat",
+            breed: "mixed"
+        })
+    ])
+
+}
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(React.createElement(App));
